0fad21291197f063cf5b2b17677fca63769e3eec
Adrian Evangelista <25916444+adriane-macer@users.noreply.github.com>
2026-05-28 23:54:01 +0800

    test: add dashboard widget tests

---
+++ test/features/wallet/presentation/pages/dashboard_page_test.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:send_money/features/wallet/presentation/cubit/wallet_cubit.dart';
+import 'package:send_money/features/wallet/presentation/pages/dashboard_page.dart';
+
+void main() {
+  testWidgets(
+    'shows visible balance initially',
+        (tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: BlocProvider(
+            create: (_) => WalletCubit(),
+            child: const DashboardPage(),
+          ),
+        ),
+      );
+
+      expect(find.text('₱500.00'), findsOneWidget);
+    },
+  );
+
+  testWidgets(
+    'hides balance when icon tapped',
+        (tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: BlocProvider(
+            create: (_) => WalletCubit(),
+            child: const DashboardPage(),
+          ),
+        ),
+      );
+
+      await tester.tap(
+        find.byIcon(Icons.visibility),
+      );
+
+      await tester.pump();
+
+      expect(find.text('******'), findsOneWidget);
+    },
+  );
+
+  testWidgets(
+    'shows balance again when icon tapped twice',
+        (tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: BlocProvider(
+            create: (_) => WalletCubit(),
+            child: const DashboardPage(),
+          ),
+        ),
+      );
+
+      await tester.tap(
+        find.byIcon(Icons.visibility),
+      );
+
+      await tester.pump();
+
+      await tester.tap(
+        find.byIcon(Icons.visibility_off),
+      );
+
+      await tester.pump();
+
+      expect(find.text('₱500.00'), findsOneWidget);
+    },
+  );
+}
