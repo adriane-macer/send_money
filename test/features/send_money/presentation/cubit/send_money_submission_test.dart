f70c5d9448b413751943fa98f70fa7a36fd4492f
Adrian Evangelista <25916444+adriane-macer@users.noreply.github.com>
2026-05-29 01:06:39 +0800

    test: add send money submission cubit tests

---
+++ test/features/send_money/presentation/cubit/send_money_submission_test.dart
@@ -0,0 +1,50 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import 'package:send_money_app/features/send_money/domain/usecases/send_money_usecase.dart';
+
+import 'package:send_money_app/features/send_money/presentation/cubit/send_money_cubit.dart';
+import 'package:send_money_app/features/send_money/presentation/cubit/send_money_state.dart';
+
+class MockSendMoneyUseCase extends Mock implements SendMoneyUseCase {}
+
+void main() {
+  late SendMoneyCubit cubit;
+  late MockSendMoneyUseCase mockUseCase;
+
+  setUp(() {
+    mockUseCase = MockSendMoneyUseCase();
+
+    cubit = SendMoneyCubit(sendMoneyUseCase: mockUseCase);
+  });
+
+  tearDown(() {
+    cubit.close();
+  });
+
+  blocTest<SendMoneyCubit, SendMoneyState>(
+    'emits loading then success when API succeeds',
+    build: () {
+      when(() => mockUseCase(amount: 100)).thenAnswer((_) async {});
+
+      return cubit;
+    },
+    act: (cubit) => cubit.submitMoney(100),
+    expect: () => [SendMoneyLoading(), SendMoneySuccess()],
+  );
+
+  blocTest<SendMoneyCubit, SendMoneyState>(
+    'emits loading then failure when API fails',
+    build: () {
+      when(() => mockUseCase(amount: 100)).thenThrow(Exception('API Error'));
+
+      return cubit;
+    },
+    act: (cubit) => cubit.submitMoney(100),
+    expect: () => [
+      SendMoneyLoading(),
+      const SendMoneyFailure('Failed to send money'),
+    ],
+  );
+}
